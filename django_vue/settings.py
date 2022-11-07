5ff7c02ef501cd08411efcc48b9c754e09b374c5
Thomas <Jia.Liang.Guo@ocean.ibm.com>
2022-11-07 17:30:39 +0800

    django_vue init

--- django_vue/settings.py
+++ django_vue/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'rest_framework',
 ]
 
 ROOT_URLCONF = 'django_vue.urls'
