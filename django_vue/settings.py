71fbf25ecc87f5a7fa133cb5a43e17af0e72ca4b
Thomas <Jia.Liang.Guo@ocean.ibm.com>
2022-11-07 20:43:13 +0800

    django_vue init

--- django_vue/settings.py
+++ django_vue/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q+eo2i#ueb$+1-6^sx=6)j1+^+t&x)3e0)gydw+cwh8o1!d4as'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
